dac683318040f3e68d76f4660990badc5dc11d19
Hugo Mercado <hugo.mario.mercado@gmail.com>
2019-06-29 15:47:39 -0500

    handled 3.0.2 version

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ var initializeMiddleware = function initializeMiddleware (rlOrSO, resources, cal
     debug('  Validation: %s', err ? 'failed' : 'succeeded');
 
     // TODO: remove this ugly hack.
-    if (spec.version === '3.0.0' || spec.version === '3.0.1') {
+    if (spec.version.startsWith('3.0') === '3.0.0' || spec.version === '3.0.1' || spec.version === '3.0.2') {
         err = null;
     }
 

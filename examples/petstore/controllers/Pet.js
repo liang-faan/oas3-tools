55bebed7e66e4b7d70ca88e9edd3e8cf7454d9f1
HugoMario <hugo.mario.mercado@gmail.com>
2019-12-21 19:37:08 -0500

    added example

---
+++ examples/petstore/controllers/Pet.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Pet = require('../services/PetService');
+
+module.exports.addPet = function addPet (req, res, next) {
+    var body = req.body;
+    Pet.addPet(body)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            utils.writeJson(res, response);
+        });
+};
+
+
+module.exports.findPetsByStatus = function findPetsByStatus (req, res, next) {
+    var status = req.query.status[0];
+    var sessionid = req.cookies.sessionId;
+
+    console.log("status:", status)
+    console.log("sessionId:", sessionid)
+
+    Pet.findPetsByStatus(status,sessionid)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            utils.writeJson(res, response);
+        });
+};
+
+module.exports.getPetById = function getPetById (req, res, next) {
+
+    var petId = req.openapi.pathParams['petId'];
+    Pet.getPetById(petId)
+        .then(function (response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function (response) {
+            utils.writeJson(res, response);
+        });
+};
+
